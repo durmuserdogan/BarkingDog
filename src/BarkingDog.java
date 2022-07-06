72ac9e277b686dc50f8b0b2ef74d69d0681beda8
durmuserdogan <drmserdogan@outlook.com>
2022-07-06 14:17:00 +0300

    first com

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,5 +1,13 @@
 public class BarkingDog {
-    public static void shouldWakeUp(boolean barkingDog, int hourOfDay[23]){
-
+    public static boolean shouldWakeUp(boolean barkingDog, int hourOfDay){
+        if(hourOfDay<0 || hourOfDay>23){
+            return false;
+        }
+        else if(barkingDog && (hourOfDay<8 || hourOfDay>22)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        boolean t =BarkingDog.shouldWakeUp(true,8);
+        System.out.println(t);
+    }
 }
